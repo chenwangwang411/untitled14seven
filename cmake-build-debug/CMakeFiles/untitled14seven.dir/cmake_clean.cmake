647e6390e19cb8ea284e2c6b875e42c9a5fdaa4e
chen'wang'wang <3043859342@qqcom>
2020-10-07 10:20:24 +0800

    第一次创建工程

---
+++ cmake-build-debug/CMakeFiles/untitled14seven.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled14seven.dir/main.c.obj"
+  "libuntitled14seven.dll.a"
+  "untitled14seven.exe"
+  "untitled14seven.exe.manifest"
+  "untitled14seven.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled14seven.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/untitled14seven.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for untitled14seven.
+# This may be replaced when dependencies are built.
